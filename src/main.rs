2d46cc17b72479bc4f1ab4c0bd0491bfb414d20e
NightCruising <2586447362@qq.com>
2021-11-03 20:10:38 +0800

    update read16 method of Memory

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,18 @@
-use std::fs::{self, File};
-use std::io;
-use std::io::Read;
 use std::path::Path;
 
+mod mem;
+mod cpu;
+mod op;
+mod chip8;
+
+use chip8::CHIP8;
+
 fn main() {
-    println!("Hello World");
-}
-
-
-const MEMORY_SIZE: usize = 4096;
-
-pub struct CHIP8 {
-    /// use an u8 array to emulate chip8 memory
-    mem: [u8; MEMORY_SIZE]
-}
-
-impl CHIP8 {
-    /// create the chip8 instance
-    pub fn new() -> CHIP8 {
-        CHIP8 {
-            mem: [0; MEMORY_SIZE]
+    if let Err(err) = CHIP8::run() {
+        println!("Application Error: {}", err);
+        if let Some(source) = err.source() {
+            println!("Source: {}", source);
         }
+        std::process::exit(1);
     }
-
-    /// load the ROM into memory
-    pub fn load_rom(&mut self, path: &Path) -> io::Result<()> {
-        // Read file
-        let rom_data = fs::read_to_string(path)?.as_bytes();
-
-        // load the ROM into memory at 0x200
-        // most chip-8 programs start at location 0x200
-        // see http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#2.1 for details
-        for (i, &data) in rom_data.iter().enumerate() {
-            self.mem[0x200+i] = data;
-        }
-
-        Ok(())
-    }
-}
-
-pub struct CPU {
-    v: [u8; 16],    // general purpose 8-bit registers(from V0 to VF, and the VF is used as a flag by some instructions)
-    i: u16,         // generally used to store memory address
-    dt: u8,         // delay timer
-    st: u8,         // sound timer
-    pc: u16,        // store the currently executing address
-    sp: u8,         // point to the topmost level of the stack
 }
