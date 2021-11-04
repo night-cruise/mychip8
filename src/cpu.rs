6ccf8d229df448a320752ad628f02a0202a1699e
NightCruising <2586447362@qq.com>
2021-11-04 11:46:50 +0800

    add some instruction implementation

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,12 +2,13 @@ use crate::mem::Memory;
 use crate::op::Op;
 
 pub struct CPU {
-    v: [u8; 16],    // general purpose 8-bit registers(from V0 to VF, and the VF is used as a flag by some instructions)
-    i: u16,         // generally used to store memory address
-    dt: u8,         // delay timer
-    st: u8,         // sound timer
-    pc: u16,        // store the currently executing address
-    sp: u8,         // point to the topmost level of the stack
+    v: [u8; 16],        // general purpose 8-bit registers(from V0 to VF, and the VF is used as a flag by some instructions)
+    i: u16,             // generally used to store memory address
+    dt: u8,             // delay timer
+    st: u8,             // sound timer
+    pc: u16,            // store the currently executing address
+    sp: u8,             // point to the topmost level of the stack
+    stack: [u16; 16]    // stack is an array of 16 16-bit values, used to store the address that the interpreter returns to when finished with a subroutine
 }
 
 impl CPU {
@@ -19,11 +20,12 @@ impl CPU {
             dt: 0,
             st: 0,
             pc: 0x200,     // chip-8 programs start at location 0x200
-            sp: 0
+            sp: 0,
+            stack: [0; 16]
         }
     }
 
-    /// fetch, decode and execute instruction cyclically
+    /// fetch, decode and execute instruction
     pub fn cycle(&mut self, memory: &mut Memory) {
         // read 2 bytes opcode at program counter
         let opcode = memory.read16(self.pc);
@@ -34,17 +36,145 @@ impl CPU {
         // decode the instruction
         let op = Op::decode(&opcode);
 
-        // parameter at different location
-        let x = Op::x(&opcode);
-        let y = Op::y(&opcode);
-        let kk = Op::kk(&opcode);
-
-        // execute the instruction
+        println!("{:04X}: {:04X}  {:?}", 0x200+self.pc, opcode.get_opcode(), op);
         match op {
-            Op::LD => self.v[x] = kk,
-            Op::ADD => self.v[x] += kk,
-            Op::LDR => self.v[x] = self.v[y],
+            Op::SYS {address}           =>      self.sys(address),
+            Op::CLS                          =>      self.cls(),
+            Op::RET                          =>      self.ret(),
+            Op::JP {address}            =>       self.jp(address),
+            Op::CALL {address}          =>       self.call(address),
+            Op::SE {reg, byte}      =>        self.se(reg, byte),
+            Op::LD {reg, byte}      =>       self.ld(reg, byte),
+            Op::ADD {reg, byte}     =>       self.add(reg, byte),
+            Op::LDR {reg_x, reg_y}  =>       self.ldr(reg_x, reg_y),
+            Op::OR {reg_x, reg_y}   =>       self.or(reg_x, reg_y),
+            Op::AND {reg_x, reg_y}  =>       self.and(reg_x, reg_y),
+            Op::XOR {reg_x ,reg_y}  =>       self.xor(reg_x, reg_y),
+            Op::ADD2 {reg_x, reg_y} =>       self.add2(reg_x, reg_y),
+            Op::SUB {reg_x, reg_y}  =>       self.sub(reg_x, reg_y),
+            Op::SHR {reg_x, reg_y}  =>       self.shr(reg_x, reg_y),
+            Op::SUBN {reg_x, reg_y} =>       self.subn(reg_x, reg_y),
+            Op::SHL {reg_x, reg_y}  =>       self.shl(reg_x, reg_y),
+            Op::LDA {address}           =>       self.lda(address),
+            Op::LDI {reg}                =>       self.ldi(reg, memory),
+            _ => todo!()
         }
     }
 
+    fn sys(&mut self, _address: u16) {
+        // doesn't need to do this
+    }
+
+    fn cls(&mut self) {
+        todo!()
+    }
+
+    // the interpreter sets the program counter to the address at the top of the stack,
+    // then subtracts 1 from the stack pointer.
+    fn ret(&mut self) {
+        self.sp -= 1;
+        self.pc = self.stack[self.sp as usize];
+    }
+
+    fn jp(&mut self, address: u16) {
+        self.pc = address;
+    }
+
+    // increments stack pointer, then puts the PC on the top of the stack. and set PC to nnn
+    fn call(&mut self, address: u16) {
+        self.stack[self.sp as usize] = self.pc;
+        self.sp += 1;
+        self.pc = address;
+    }
+
+    fn se(&mut self, reg: u8, byte: u8) {
+        if self.v[reg as usize] == byte {
+            self.pc += 2;
+        }
+    }
+
+    fn ld(&mut self, reg: u8, byte: u8) {
+        self.v[reg as usize] = byte;
+    }
+
+    fn add(&mut self, reg: u8, byte: u8) {
+        self.v[reg as usize] += byte;
+    }
+
+    fn ldr(&mut self, reg_x: u8, reg_y: u8) {
+        self.v[reg_x as usize] = self.v[reg_y as usize];
+    }
+
+    fn or(&mut self, reg_x: u8, reg_y: u8) {
+        self.v[reg_x as usize] = self.v[reg_x as usize] | self.v[reg_y as usize];
+    }
+
+    fn and(&mut self, reg_x: u8, reg_y: u8) {
+        self.v[reg_x as usize] = self.v[reg_x as usize] & self.v[reg_y as usize];
+    }
+
+    fn xor(&mut self, reg_x: u8, reg_y: u8) {
+        self.v[reg_x as usize] = self.v[reg_x as usize] ^ self.v[reg_y as usize];
+    }
+
+    // add Vx and Vy.
+    // if the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0.
+    // only the lowest 8 bits of the result are kept, and stored in Vx.
+    fn add2(&mut self, reg_x: u8, reg_y: u8) {
+        let (result, carry) = self.v[reg_x as usize].overflowing_add(self.v[reg_y as usize]);
+        self.v[reg_x as usize] = result;
+        self.v[0xF] = if carry { 1 } else { 0 };
+    }
+
+    // if Vx > Vy, then VF is set to 1, otherwise 0.
+    // Then Vy is subtracted from Vx, and the results stored in Vx.
+    fn sub(&mut self, reg_x: u8, reg_y: u8) {
+        let (result, borrow) = self.v[reg_x as usize].overflowing_sub(self.v[reg_y as usize]);
+        self.v[reg_x as usize] = result;
+        self.v[0xF] = if borrow { 0 } else { 1 };
+    }
+
+    // if the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0.
+    // then Vx is divided by 2.
+    fn shr(&mut self, reg_x: u8, reg_y: u8) {
+        self.v[0xF] = self.v[reg_x as usize] & 1;
+        self.v[reg_x as usize] = self.v[reg_x as usize] >> 1;
+    }
+
+    // if Vy > Vx, then VF is set to 1, otherwise 0.
+    // then Vx is subtracted from Vy, and the results stored in Vx.
+    fn subn(&mut self, reg_x: u8, reg_y: u8) {
+        let (result, borrow) = self.v[reg_y as usize].overflowing_sub(self.v[reg_x as usize]);
+        self.v[reg_x as usize] = result;
+        self.v[0xF] = if borrow { 0 } else { 1 };
+    }
+
+    // if the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0.
+    // then Vx is multiplied by 2.
+    fn shl(&mut self, reg_x: u8, reg_y: u8) {
+        self.v[0xF] = if let 0b10000000 = self.v[reg_x as usize] & 0b10000000 {
+            1
+        } else {
+            0
+        };
+        self.v[reg_x as usize] = self.v[reg_x as usize] << 1;
+    }
+
+    // if Vx != Vy, the program counter is increased by 2.
+    fn sne(&mut self, reg_x: u8, reg_y: u8) {
+        if self.v[reg_x as usize] != self.v[reg_y as usize] {
+            self.pc += 2;
+        }
+    }
+
+    fn lda(&mut self, address: u16) {
+        self.i = address;
+    }
+
+    fn ldi(&mut self, reg: u8, memory: &mut Memory) {
+        for i in 0..=reg {
+            let address = (self.i + i as u16) as usize;
+            memory.write(address, self.v[i as usize]);
+        }
+    }
 }
--- src/op.rs
+++ src/op.rs
@@ -1,4 +1,5 @@
 /// store opcode value
+#[derive(Debug)]
 pub struct OpCode(u16);
 
 impl OpCode {
@@ -14,35 +15,212 @@ impl OpCode {
 }
 
 /// opcode type
+#[derive(Debug)]
 pub enum Op {
-    LD,     // opcode: 6xkk, set Vx to kk
-    ADD,    // opcode: 7xkk, add kk to Vx
-    LDR,    // opcode: 8xy0, set Vx to Vy
+    SYS {address: u16},                 // opcode: 0nnn, jump to a machine code routine at nnn
+    CLS,                                // opcode: 00E0, clear the display
+    RET,                                // opcode: 00EE, return from a subroutine
+    JP  {address: u16},                 // opcode: 1nnn, set PC to nnn
+    CALL{address: u16},                 // opcode: 2nnn, call subroutine at nnn
+    SE  {reg: u8, byte: u8},            // opcode: 3xkk, skip next instruction if Vx = kk
+    LD  {reg: u8, byte: u8},            // opcode: 6xkk, set Vx to kk
+    ADD {reg: u8, byte: u8},            // opcode: 7xkk, add kk to Vx
+    LDR {reg_x: u8, reg_y: u8},         // opcode: 8xy0, set Vx to Vy
+    OR  {reg_x: u8, reg_y: u8},         // opcode: 8xy1, set Vx = Vx OR Vy
+    AND {reg_x: u8, reg_y: u8},         // opcode: 8xy2, set Vx = Vx AND Vy
+    XOR {reg_x: u8, reg_y: u8},         // opcode: 8xy3, set Vx = Vx XOR Vy
+    ADD2{reg_x: u8, reg_y: u8},         // opcode: 8xy4, set Vx = Vx + Vy, set VF = carry
+    SUB {reg_x: u8, reg_y: u8},         // opcode: 8xy5, set Vx = Vx - Vy, set VF = NOT borrow
+    SHR {reg_x: u8, reg_y: u8},         // opcode: 8xy6, set Vx = Vx SHR 1
+    SUBN{reg_x: u8, reg_y: u8},         // opcode: 8xy7, set Vx = Vy - Vx, set VF = NOt borrow
+    SHL {reg_x: u8, reg_y: u8},         // opcode: 8xyE, set Vx = Vx SHL 1
+    SNE {reg_x: u8, reg_y: u8},         // opcode: 9xy0, skip next instruction if Vx != Vy.
+    LDA {address: u16},                 // opcode: Annn, set I to nnn
+    LDI {reg: u8},                      // opcode: Fx55, store registers V0 through Vx in memory starting at location I.
 }
 
 impl Op {
     /// decode the type of opcode
     pub fn decode(opcode: &OpCode) -> Op {
-        match opcode.get_opcode() & 0xF000 {
-            0x6000 => Op::LD,
-            0x7000 => Op::ADD,
-            0x8000 => Op::LDR,
-            _ => panic!("invalid opcode: {:04X}", opcode.get_opcode()),
+        let opcode = opcode.get_opcode();
+        match opcode & 0xF000 {
+            0x0000 => {
+              match opcode & 0x0FFF {
+                  0x00E0 => Op::cls(),
+                  0x00EE => Op::ret(),
+                  _ => Op::sys(opcode)
+              }
+            },
+            0x1000 => Op::jp(opcode),
+            0x2000 => Op::call(opcode),
+            0x3000 => Op::se(opcode),
+            0x6000 => Op::ld(opcode),
+            0x7000 => Op::add(opcode),
+            0x8000 => {
+                match opcode & 0x000F {
+                    0x0000 => {
+                        Op::ldr(opcode)
+                    },
+                    0x0001 => {
+                        Op::or(opcode)
+                    },
+                    0x0002 => {
+                        Op::and(opcode)
+                    },
+                    0x0003 => {
+                        Op::xor(opcode)
+                    },
+                    0x0004 => {
+                        Op::add2(opcode)
+                    },
+                    0x0005 => {
+                        Op::sub(opcode)
+                    },
+                    0x0006 => {
+                        Op::shr(opcode)
+                    },
+                    0x0007 => {
+                        Op::subn(opcode)
+                    },
+                    0x000E => {
+                        Op::shl(opcode)
+                    },
+                    _ => panic!("invalid opcode: {:04X}", opcode),
+                }
+            },
+            0x9000 => Op::sne(opcode),
+            0xA000 => Op::lda(opcode),
+            0xF000 => Op::ldi(opcode),
+            _ => panic!("invalid opcode: {:04X}", opcode),
         }
     }
 
-    /// x in opcode 6xkk
-    pub fn x(opcode: &OpCode) -> usize {
-        ((opcode.get_opcode() & 0x0F00) >> 8) as usize
+    fn sys(opcode: u16) -> Op {
+        Op::SYS {
+            address: opcode & 0x0FFF
+        }
     }
 
-    /// y in opcode 8xy0
-    pub fn y(opcode: &OpCode) -> usize {
-        ((opcode.get_opcode() & 0x00F0) >> 4) as usize
+    fn cls() -> Op {
+        Op::CLS
     }
 
-    /// kk in opcode 7xkk
-    pub fn kk(opcode: &OpCode) -> u8 {
-        ((opcode.get_opcode() & 0x00FF)) as u8
+    fn ret() -> Op {
+        Op::RET
+    }
+
+    fn jp(opcode: u16) -> Op {
+        Op::JP {
+            address: opcode & 0x0FFF
+        }
+    }
+
+    fn call(opcode: u16) -> Op {
+        Op::CALL {
+            address: opcode & 0x0FFF
+        }
+    }
+
+    fn se(opcode: u16) -> Op {
+        Op::SE {
+            reg: ((opcode & 0x0F00) >> 8) as u8,
+            byte: (opcode & 0x00FF) as u8
+        }
+    }
+
+    fn ld(opcode: u16) -> Op {
+        Op::LD {
+            reg: ((opcode & 0x0F00) >> 8) as u8,
+            byte: (opcode & 0x00FF) as u8
+        }
+    }
+
+    fn add(opcode: u16) -> Op {
+        Op::ADD {
+            reg: ((opcode & 0x0F00) >> 8) as u8,
+            byte: (opcode & 0x00FF) as u8
+        }
+    }
+
+    fn ldr(opcode: u16) -> Op {
+        Op::LDR {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8
+        }
+    }
+
+    fn or(opcode: u16) -> Op {
+        Op::OR {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn and(opcode: u16) -> Op {
+        Op::AND {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn xor(opcode: u16) -> Op {
+        Op::XOR {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn add2(opcode: u16) -> Op {
+        Op::ADD2 {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn sub(opcode: u16) -> Op {
+        Op::SUB {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn shr(opcode: u16) -> Op {
+        Op::SHR {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn subn(opcode: u16) -> Op {
+        Op::SUBN {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn shl(opcode: u16) -> Op {
+        Op::SHL {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn sne(opcode: u16) -> Op {
+        Op::SNE {
+            reg_x: ((opcode & 0x0F00) >> 8) as u8,
+            reg_y: ((opcode & 0x00F0) >> 4) as u8,
+        }
+    }
+
+    fn lda(opcode: u16) -> Op {
+        Op::LDA {
+            address: opcode & 0x0FFF
+        }
+    }
+
+    fn ldi(opcode: u16) -> Op {
+        Op::LDI {
+            reg: ((opcode & 0x0F00) >> 8) as u8
+        }
     }
 }
