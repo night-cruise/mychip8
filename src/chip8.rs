cae6e1d51785cc5319bd167a548bbb14ca79ef0d
NightCruising <2586447362@qq.com>
2021-11-09 22:28:11 +0800

    update chip8.rs

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,6 +1,9 @@
-use std::path::Path;
 use crate::cpu::CPU;
-use crate::mem::Memory;
+use crate::display::Display;
+use crate::keyboard::KeyBoard;
+use crate::memory::Memory;
+use std::path::Path;
+use crate::settings::Settings;
 
 const GAME_FILE: &str = "c8games/BLINKY";
 
@@ -13,9 +16,17 @@ impl CHIP8 {
         let mut memory = Memory::new();
         memory.load_rom(Path::new(GAME_FILE))?;
 
+        let mut display = Display::new();
+
+        let mut keyboard = KeyBoard::new();
+
         let mut cpu = CPU::new();
 
-        cpu.cycle(&mut memory);
+        let settings = Settings::new();
+
+        for _i in 0..500 {
+            cpu.cycle(&mut memory, &mut display, &mut keyboard, &settings);
+        }
 
         Ok(())
     }
