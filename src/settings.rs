2c67b905189037f6a80f1973a8707f58d5af9e7e
NightCruising <2586447362@qq.com>
2021-11-21 21:57:37 +0800

    modify comments

--- src/settings.rs
+++ src/settings.rs
@@ -1,20 +1,23 @@
 /// chip8 settings
 pub struct Settings {
-    pub cpu_freq: u16,         // cpu frequency
-    pub delay_timer_freq: u16, // delay timer reduce frequency
-    pub sound_timer_freq: u16, // sound timer reduce frequency
+    pub cpu_freq: u16,
+    /// cpu frequency
+    pub delay_timer_freq: u16,
+    /// delay timer reduce frequency
+    pub sound_timer_freq: u16,
+    /// sound timer reduce frequency
 
-    // in some games, chip8 needs to increment the I register after executing LDI(FX55) and LDJ(FX65) instructions
+    /// in some games, chip8 needs to increment the I register after executing LDI(FX55) and LDJ(FX65) instructions
     pub increment_i_register: bool,
-    // in some games, chip8 needs to shift Vx and ignore Vy while executing SHR(8xy6) and SHL(8xyE) instructions
+    /// in some games, chip8 needs to shift Vx and ignore Vy while executing SHR(8xy6) and SHL(8xyE) instructions
     pub shift_vx_ignore_vy: bool,
-    // in some games, chip8 needs to set the Vf register when overflow occurs while executing ADDI(FX1E) instruction
+    /// in some games, chip8 needs to set the Vf register when overflow occurs while executing ADDI(FX1E) instruction
     pub set_vf_when_overflow: bool,
 
     /// some games don't need sound
     pub mute: bool,
 
-    // if the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen
+    /// if the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen
     pub vertical_wrap: bool,
 }
 
